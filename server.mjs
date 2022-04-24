6f556866846a40d4426f52e8d39fad77f27371d5
sarrahman <sarrahman.dev@gmail.com>
2022-04-24 14:32:29 +0800

    use Algolia search

--- server.mjs
+++ server.mjs
@@ -3,8 +3,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./routes/index.mjs";
+import algoliasearch from "algoliasearch";
+import cron from "node-cron";
 
 dotenv.config();
+
+const client = algoliasearch(
+  process.env.ALGOLIA_ID,
+  process.env.ALGOLIA_API_KEY
+);
+const index = client.initIndex("all-products");
 
 try {
   mongoose.connect(process.env.MONGO_CONNECTION);
@@ -19,6 +27,23 @@ try {
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+cron.schedule("*/5 * * * *", () => {
+  mongoose.connect(process.env.MONGO_CONNECTION);
+  const db = mongoose.connection;
+  db.collection("products")
+    .find()
+    .toArray(function (err, result) {
+      if (err) throw err;
+      result.forEach((product) => {
+        product.objectID = product._id;
+        index.saveObject(product, function (err) {
+          if (err) throw err;
+        });
+      });
+    });
+  console.log("Products indexed in Algolia");
+});
 
 process.on("unhandledRejection", (reason, promise) => {
   console.log("Unhandled Rejection at:", reason.stack || reason);
